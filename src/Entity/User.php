a38d6273ed4ccd2f137cc6484dc4e2a48d12a110
DaitoCK <72608496+DaitoCK@users.noreply.github.com>
2021-03-03 15:35:49 +0100

    Display pin owner and add getFullName method for user's full name

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -194,4 +194,10 @@ class User implements UserInterface
 
         return $this;
     }
+
+    public function getFullName(): string
+    {
+        return $this->getFirstName() .' '. $this->getLastName();
+    }
+
 }
