e3a7cb959cff2c5b29f38c5b7e6ec8b9d9bc31c6
DaitoCK <72608496+DaitoCK@users.noreply.github.com>
2021-03-15 19:17:38 +0100

    Display add message logout

--- src/EventSubscriber/LogoutEventSubscriber.php
+++ src/EventSubscriber/LogoutEventSubscriber.php
@@ -5,24 +5,25 @@ namespace App\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
-use Symfony\Component\Routing\Generator\UrlGenerator;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 class LogoutEventSubscriber implements EventSubscriberInterface
 {
     private $urlGenerator;
+    private $flashbag;
 
     public function __construct(FlashBagInterface $flashBag, UrlGeneratorInterface $urlGenerator)
     {
         $this->urlGenerator = $urlGenerator;
+        $this->flashbag = $flashBag;
     }
 
     public function onLogoutEvent(LogoutEvent $event)
     {
-        $event->getRequest()->getSession()->getFlashBag->add(
+        $this->flashbag->add(
             'success',
-            'Deconnecté avec succès'
+            'Bye bye '. $event->getToken()->getUser()->getFullName()
         );
 
         $event->setResponse(new RedirectResponse($this->urlGenerator->generate('app_home')));
