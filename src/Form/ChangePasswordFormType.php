ba6f080404d87e9afad43d7b406358e88925c480
DaitoCK <72608496+DaitoCK@users.noreply.github.com>
2021-03-15 16:14:44 +0100

    more robust change Password form

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -7,34 +7,53 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+
 
 class ChangePasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        if ($options['current_password_is_required']) {
+            $builder
+                ->add('currentPassword', PasswordType::class, [
+                    'label' => 'Mot de passe acutel',
+                    'attr' => [
+                        'autocomplete' => 'off'
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez saisir votre mot de passe actuel',
+                        ]),
+                        new UserPassword(['message' => 'Mot de passe actuel non valide.']),
+                    ]
+                ]);
+        }
+
         $builder
-            ->add('newPassword', RepeatedType::class, [
+            ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => [
                     'constraints' => [
                         new NotBlank([
-                            'message' => 'Veuillez entrer votre mot de passe',
+                            'message' => 'Please enter a password',
                         ]),
                         new Length([
                             'min' => 6,
-                            'minMessage' => 'Votre mot de passe doit comporter au moins {{limit}} caractères',
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
                             // max length allowed by Symfony for security reasons
                             'max' => 4096,
                         ]),
                     ],
                     'label' => 'Nouveau mot de passe',
+                    'attr'=> array('class'=>'mb-3')
                 ],
                 'second_options' => [
-                    'label' => 'Confirmer nouveau mot de passe',
+                    'label' => 'Confirmer mot de passe',
                 ],
-                'invalid_message' => 'Les champs du mot de passe doivent correspondre.',
+                'invalid_message' => 'The password fields must match.',
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
@@ -44,6 +63,10 @@ class ChangePasswordFormType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([]);
+        $resolver->setDefaults([
+            'current_password_is_required'=> false
+        ]);
+
+        $resolver->setAllowedTypes('current_password_is_required', 'bool');
     }
 }
